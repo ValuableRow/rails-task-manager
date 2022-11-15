d2c81ce16c6207b352fae2de8f4108f99f418d38
Christopher Beraud <beraud.christopher@gmail.com>
2022-11-15 12:57:30 +0100

    add new task

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get 'tasks', to: 'tasks#index'
+  # READ all
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # READ one
+  get 'tasks/:id', to: 'tasks#show', as: :task
   # root "articles#index"
 end
